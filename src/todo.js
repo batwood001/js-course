73fc94940b97a7805e645f16bedd77fed7859f34
Brian Atwood <batwood011@gmail.com>
2015-01-14 16:57:03 -0600

    make some buttons

--- src/todo.js
+++ src/todo.js
@@ -2,12 +2,59 @@
 	
 	window.TodoApp = {}
 
+	// View-Model
+	
+	TodoApp.vm = {
+		todos: m.prop([])
+	}
+
+	TodoApp.find = function (id) {
+		var todos = TodoApp.vm.todos()
+		for (var i = 0; i < todos.length; i++) {
+			if (todos[i].id === id) return todos[i]
+		}
+	}
+
 	TodoApp.controller = function () {
 		var ctrl = {}
+		
+		ctrl.updateTodo = function (id, isComplete) {
+			console.log("Updated")
+			var todo = TodoApp.find(id)
+			todo.isComplete = isComplete
+		}
+
+		ctrl.flip = function () {
+			TodoApp.vm.todos().forEach(function(todo) {
+				todo.isComplete = !todo.isComplete
+			})
+		}
 		return ctrl
 	}
 
 	TodoApp.view = function (ctrl) {
-		return m('.todos', "Hello world")
+		return m('.todos', [
+			TodoApp.vm.todos().map(todoView),
+			m('button', {onclick: ctrl.flip}, "Flip All Todos")
+			
+		])
+
+		function todoView (todo) {
+			return m('.todo', [
+				m('input[type=checkbox]', {
+					checked: todo.isComplete,
+					// function (id, isComplete)
+					onchange: ctrl.updateTodo.bind(null, todo.id, !todo.isComplete),
+
+					onchange: function (e) {
+						ctrl.updateTodo(todo.id, !todo.isComplete)
+					}
+				}),
+				m('label', todo.name)
+				// Checkbox
+				// Label
+				// Bind click action to controller
+			])
+		}
 	}
 })()
