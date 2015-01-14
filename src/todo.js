e0c7d77b660767fc630e954c35dda0aeeee876f2
Brian Atwood <batwood011@gmail.com>
2015-01-14 15:26:06 -0600

    mithril boilerplate

---
+++ src/todo.js
@@ -0,0 +1,13 @@
+(function () {
+	
+	window.TodoApp = {}
+
+	TodoApp.controller = function () {
+		var ctrl = {}
+		return ctrl
+	}
+
+	TodoApp.view = function (ctrl) {
+		return m('.todos', "Hello world")
+	}
+})()
